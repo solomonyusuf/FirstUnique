809316712d68aef89c6fcb7bc38fbfec03a1c570
olafire <olafire03@gmail.com>
2022-08-28 13:21:46 +0100

    Add project files.

---
+++ FirstUnique/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] s = { 1, 4, 3, 3, 1, 2 };
+Solution(s);
+
+int Solution(int[] A)
+{
+    var value = 0;
+    var duplicate = A.GroupBy(x => x).Where(d => d.Count() == 1);
+    if (duplicate.Any())
+    {
+        var a = duplicate.ToArray();
+        var res = a.First().Key;
+        return res += value;
+    }
+    else
+    {
+        return -1;
+    }
+
+}
+
